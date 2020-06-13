8932fe333453eefe5f5e00368c32f55996635233
Fragmnt X <fragmnt@x.com>
2020-06-13 00:49:14 -0400

    First commit

---
+++ src/Index.cs
@@ -0,0 +1,10 @@
+using Stratis.SmartContract;
+
+[Deploy]
+
+public class ShippingWatch : SmartContract {
+    public struct data {};
+    private const string AppName = "ShipFast";
+
+    public void className(type value) {}
+};
